cace0615495513e07692e446f4c6d300daabbdb7
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2018-06-23 16:33:01 -0600

    updates for the new page

--- header.php
+++ header.php
@@ -16,45 +16,31 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
-
 		<?php wp_head(); ?> </head>
 
 	<body <?php body_class(); ?>>
 		<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 		<?php endif; ?>
-		<header class="site-header" role="banner">
-			<div class="grid-container">
-				<div class="grid-x">
-					<div class="medium-8 medium-order-2 small-12 cells">
-						<div class="monstertext-container">
-							<img src="/monsterswitch-wp/wp-content/uploads/2018/04/monsterswitch-text-light.png" alt="MonsterSwitch Logo Text" class="logo-text"> </div.>
-						</div>
-					</div>
-					<div class="medium-4 medium-order-1 small-12 cells">
-						<div class="monster-container">
-							<img src="/monsterswitch-wp/wp-content/uploads/2018/04/monster.png" alt="MonsterSwitch Logo" class="logo" id="logo"> </div>
-					</div>
-				</div>
-			</div>
-			<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-				<div class="title-bar-left">
-					<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-					<span class="site-mobile-title title-bar-title">
-						<!-- <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
+			<div class="title-bar-left">
+				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+				<span class="site-mobile-title title-bar-title">
+					<!-- <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 							<?php bloginfo( 'name' ); ?>
 						</a> -->
-					</span>
-				</div>
+				</span>
 			</div>
-			<nav class="site-navigation top-bar" role="navigation">
-				<!-- <div class="top-bar-left">
+		</div>
+		<nav class="site-navigation top-bar" role="navigation">
+				<div class="top-bar-left">
 						<div class="site-desktop-title top-bar-title">
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-								<?php bloginfo( 'name' ); ?>
+							<img src="/monsterswitch-wp/wp-content/uploads/2018/04/monster.png" alt="MonsterSwitch Logo" class="nav-logo">
+							<img src="/monsterswitch-wp/wp-content/uploads/2018/04/monsterswitch-text-light.png" alt="MonsterSwitch Logo Text" class="nav-logo-text">
 							</a>
 						</div>
-					</div> -->
+					</div>
 				<div class="top-bar-right">
 					<?php foundationpress_top_bar_r(); ?>
 					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
--- page-templates/front.php
+++ page-templates/front.php
@@ -4,6 +4,32 @@ Template Name: Front
 */
 get_header(); ?>
 
+<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
+  <div class="orbit-wrapper">
+    <div class="orbit-controls">
+      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+      <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+    </div>
+    <ul class="orbit-container">
+      <li class="is-active orbit-slide">
+        <figure class="orbit-figure">
+          <img class="orbit-image" src="http://localhost:8888/monsterswitch-wp/wp-content/uploads/2018/06/space-suggestion.jpg" alt="Space">
+          <!-- <figcaption class="orbit-caption">Space, the final frontier.</figcaption> -->
+        </figure>
+      </li>
+      <li class="orbit-slide">
+        <figure class="orbit-figure">
+          <img class="orbit-image" src="http://localhost:8888/monsterswitch-wp/wp-content/uploads/2018/06/fire-room-suggestion.jpg" alt="fire">
+          <!-- <figcaption class="orbit-caption">Lets Rocket!</figcaption> -->
+        </figure>
+      </li>
+    </ul>
+  </div>
+  <nav class="orbit-bullets">
+    <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+    <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+  </nav>
+</div>
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <section class="intro" role="main">
