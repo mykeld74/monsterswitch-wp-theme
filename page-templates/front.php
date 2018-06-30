c1c241aadf4bc430522e363ac11410c88b776ad4
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2018-06-30 15:55:20 -0600

    front page updates

--- page-templates/front.php
+++ page-templates/front.php
@@ -7,8 +7,12 @@ get_header(); ?>
 <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
   <div class="orbit-wrapper">
     <div class="orbit-controls">
-      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-      <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span><i class="fas fa-chevron-left carousel-control"></i>
+
+</button>
+      <button class="orbit-next"><span class="show-for-sr">Next Slide</span><i class="fas fa-chevron-right carousel-control"></i>
+
+</button>
     </div>
     <ul class="orbit-container">
       <li class="is-active orbit-slide">
